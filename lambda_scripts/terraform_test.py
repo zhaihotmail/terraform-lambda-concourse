a7c15b640270bc1486df7a7a0400d43351d8247a
jjj <zhaizhongping@hotmail.com>
2022-08-01 06:45:14 -0700

    ignore files
    changed lambda code, see if it can be deployed

--- lambda_scripts/terraform_test.py
+++ lambda_scripts/terraform_test.py
@@ -8,3 +8,4 @@ s3 = boto3.resource('s3')
 def lambda_handler(event, context):
     s3.Bucket('x').download_file("abc.csv", "/tmp/example.csv")
     boto3.resource('s3').Bucket('x').upload_file("/tmp/example.csv", 'teraform_test/' + "example.csv")
+    print("I made some change here")
