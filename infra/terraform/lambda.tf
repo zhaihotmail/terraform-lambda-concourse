74c1dca4a01ad7d557ead323af40cd238336da2d
jjj <zhaizhongping@hotmail.com>
2022-08-01 21:46:19 -0700

    Update lambda.tf

--- infra/terraform/lambda.tf
+++ infra/terraform/lambda.tf
@@ -15,7 +15,6 @@ resource "aws_lambda_function" "teraform_test" {
   #s3_bucket = "two00434-b"
   #s3_key    = "zips/terraform_test_abcd.zip"            #this is from build.sh which created terraform_test.zip
   #source_code_hash = "${base64sha256(file("./build/terraform_test_abcd.zip"))}"  # local path from where zip is uploaded to s3
-  #triggers = random_id.random.hex
   # "main" is the filename within the zip file (main.js) and "handler"
   # is the name of the property under which the handler function was
   # exported in that file.
@@ -25,7 +24,7 @@ resource "aws_lambda_function" "teraform_test" {
   timeout = "600"
   memory_size = 3008
   role = "arn:aws:iam::557563337688:role/zhai-lambda-role"
-  source_code_hash = filebase64sha256("./source_code/zipped/terraform_test.zip")
+  #source_code_hash = filebase64sha256("./source_code/zipped/terraform_test.zip")
   environment {
     variables = {
       api_key1="abcdefeg123455",
