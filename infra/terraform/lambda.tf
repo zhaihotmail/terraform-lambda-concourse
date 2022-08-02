63883b13e63753fe71e68af5b221607e5415de0e
jjj <zhaizhongping@hotmail.com>
2022-08-01 21:51:07 -0700

    add layer

--- infra/terraform/lambda.tf
+++ infra/terraform/lambda.tf
@@ -14,10 +14,6 @@ resource "aws_lambda_function" "teraform_test" {
   function_name = "test3"
   #s3_bucket = "two00434-b"
   #s3_key    = "zips/terraform_test_abcd.zip"            #this is from build.sh which created terraform_test.zip
-  #source_code_hash = "${base64sha256(file("./build/terraform_test_abcd.zip"))}"  # local path from where zip is uploaded to s3
-  # "main" is the filename within the zip file (main.js) and "handler"
-  # is the name of the property under which the handler function was
-  # exported in that file.
   filename="./source_code/zipped/terraform_test.zip"
   handler = "lambda_function.lambda_handler"
   runtime = "python3.8"
@@ -31,4 +27,5 @@ resource "aws_lambda_function" "teraform_test" {
       api_key2="asdfasdfgafgk354"
     }
   }
+  layers = ["arn:aws:lambda:us-west-1:336392948345:layer:AWSDataWrangler-Python38:7"]
 }
